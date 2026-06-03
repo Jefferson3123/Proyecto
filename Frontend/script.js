b3cc2db85024f9b093ac806a6261c707258d97be
Jefferson3123 <jefferandrey2000@gmail.com>
2026-06-03 10:55:10 -0500

    Add files via upload

---
+++ Frontend/script.js
@@ -0,0 +1,73 @@
+console.log("script cargado");
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const formulario = document.getElementById("container-login");
+
+    if (!formulario) {
+        console.log("Esta página no tiene login");
+        return;
+    }
+
+    console.log("Formulario encontrado");
+
+    formulario.addEventListener("submit", async (e) => {
+
+        e.preventDefault();
+
+        const usuario = document.getElementById("Usuario").value.trim();
+        const password = document.getElementById("Contraseña").value.trim();
+
+        try {
+
+            const respuesta = await fetch(
+                "http://localhost:3000/api/auth/login",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        usuario,
+                        password
+                    })
+                }
+            );
+
+            const datos = await respuesta.json();
+
+            console.log(datos);
+
+            if (datos.success) {
+
+                alert("Bienvenido " + datos.usuario);
+
+                localStorage.setItem(
+                    "usuario",
+                    datos.usuario
+                );
+
+                localStorage.setItem(
+                    "rol",
+                    datos.rol
+                );
+
+                window.location.href = "index.html";
+
+            } else {
+
+                alert(datos.message);
+
+            }
+
+        } catch (error) {
+
+            console.error(error);
+
+            alert("Error al conectar con el servidor");
+
+        }
+
+    });
+
+});
